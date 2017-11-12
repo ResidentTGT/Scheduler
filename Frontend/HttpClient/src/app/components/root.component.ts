693875ef50a3679ba0b9e1a0be2512e61f6a6f0d
Anatoly <anatoly.romanov@qoollo.com>
2017-11-12 19:06:52 +0300

    added HttpClient,HttpServer, .gitignore

---
+++ Frontend/HttpClient/src/app/components/root.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sch-root',
+    templateUrl: './root.component.html',
+    styleUrls: ['./root.component.css']
+})
+export class RootComponent {
+    title = 'app';
+}
